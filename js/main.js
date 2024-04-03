2479f54981bfba1d68bb819d1fb0a64efd6dce24
Owen Coombs <owencoombs03@gmail.com>
2024-04-03 15:27:13 -0400

    created building blocks
    created main functions with moving between pages

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 // main.js
 
-function init(){
-  console.log("hello world!");
-}
 
-init()
+function show(shown, hidden1, hidden2, hidden3) {
+  document.getElementById(shown).style.display = 'block';
+  document.getElementById(hidden1).style.display = 'none';
+  document.getElementById(hidden2).style.display = 'none';
+  document.getElementById(hidden3).style.display = 'none';
+  return false;
+}
---
+++ psudo.md
@@ -0,0 +1,5 @@
+# Mind Reader PSUDO
+- create and style the first page
+- figure out how to make that first page hidden 
+- create a button to take you to the next page in java 
+- 
