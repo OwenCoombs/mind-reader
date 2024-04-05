9b9c187ecbcf54767652a2068c247a5580748684
Owen Coombs <owencoombs03@gmail.com>
2024-04-05 08:23:13 -0400

    finished raandomize

--- js/main.js
+++ js/main.js
@@ -1,10 +1,57 @@
 // main.js
 
 
-function show(shown, hidden1, hidden2, hidden3) {
+function show(shown, hidden1, hidden2, hidden3, hidden4, hidden5) {
   document.getElementById(shown).style.display = 'block';
   document.getElementById(hidden1).style.display = 'none';
   document.getElementById(hidden2).style.display = 'none';
   document.getElementById(hidden3).style.display = 'none';
+  document.getElementById(hidden4).style.display = 'none';
+  document.getElementById(hidden5).style.display = 'none';
   return false;
 }
+
+
+
+
+// creating div by 9
+
+const myArray = ["!", "+", "#", "$", "%", "^"];
+console.log(myArray.length);
+function randomSymbol(){
+   return Math.floor(Math.random() * myArray.length )
+}
+
+
+
+function listNumbersWithSymbol(divisor, symbol) {
+  let outputHtml = '';
+
+  for (let i = 0; i <= 99; i++) {
+    let output = i;
+    if (i % divisor === 0) {
+      output += " " + symbol;
+    } else {
+      output += " " + myArray[randomSymbol()];
+
+    }
+    outputHtml += '<p>' + output + '</p>'; // Append each output to the HTML content
+  }
+  document.getElementById('numberContainer').innerHTML = outputHtml; // Set the generated HTML content to the numberContainer <div>
+  document.getElementById('symboldiv').innerHTML = symbol;
+}
+
+
+
+listNumbersWithSymbol(9, myArray[randomSymbol()])
+
+
+
+
+
+
+
+
+
+
+
--- psudo.md
+++ psudo.md
@@ -2,4 +2,10 @@
 - create and style the first page
 - figure out how to make that first page hidden 
 - create a button to take you to the next page in java 
-- 
+- create vars for randomizing numbers and symbols
+-
+
+
+
+# Links
+- https://stackoverflow.com/questions/6242976/javascript-hide-show-element
